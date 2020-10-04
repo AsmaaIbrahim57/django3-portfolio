6264796e0f1ec3b89ebc54d08f5794bec5b17e26
Asmaa <asmaa.ibrahim.k@gmail.com>
2020-10-04 09:31:30 +0000

    remove gitignore cache

--- new_personal_portfolio/settings.py
+++ new_personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#_^8d$yq-_1ccr5t0x6t2!lv7h_3e%1=e-gevv4)fo54%86q+a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['asmaaibrahim.pythonanywhere.com']
 
 
 # Application definition
@@ -120,5 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+
+
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
